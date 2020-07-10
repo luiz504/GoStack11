e14d6dd7b54a483be199de6f0b91e92c21232f2f
luiz504 <luiz504@hotmail.com>
2020-07-10 19:37:09 -0300

    basic json/ routes comunication

--- GoBarber/backend/src/server.ts
+++ GoBarber/backend/src/server.ts
@@ -3,8 +3,9 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log("Server on on Port 3333");
+  console.log("Server on on Port 3333"); //eslint-disable-line
 });
