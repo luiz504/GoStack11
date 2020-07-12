e164025f84b63e3a9a01166bee9ddd34019fec01
luiz504 <luiz504@hotmail.com>
2020-07-12 18:46:09 -0300

    serving static files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,10 +6,12 @@ import cors from "cors";
 import routes from "./routes";
 
 import "./database";
+import uploadConfig from "./config/upload";
 
 const app = express();
 
 app.use(express.json());
+app.use("/files", express.static(uploadConfig.directory));
 app.use(routes);
 app.use(cors());
 
