e7d72a207f4ec55d03a5da1463256e6523bdbf37
luiz504 <luiz504@hotmail.com>
2020-07-19 18:49:42 -0300

    basic yup validation

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import GlobalStyles from './styles/global';
-import SignIn from './pages/SignIn';
+import SignIn from './pages/SignIn'; //eslint-disable-line
 import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useCallback } from 'react';
+import * as Yup from 'yup';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
 import { WrapperSignIn, Content, BackGround, Form } from './styles';
@@ -9,9 +9,29 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: string): void {
-    console.log('data', data); //eslint-disable-line
+  interface Idata {
+    name: string;
+    email: string;
+    password: string;
   }
+
+  const handleSubmit = useCallback(async (data: Idata) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        email: Yup.string()
+          .required('E-mail is required')
+          .email('Type a valid email'),
+        password: Yup.string().min(6, 'Minimum size 6 characteres'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log('error', error); //eslint-disable-line
+    }
+    console.log('data', data); //eslint-disable-line
+  }, []);
 
   return (
     <WrapperSignIn>
@@ -27,7 +47,7 @@ const SignUp: React.FC = () => {
 
           <Input name="password" icon={FiLock} placeholder="password" />
 
-          <Button type="button"> Register </Button>
+          <Button type="submit"> Register </Button>
         </Form>
 
         <a href="/Sign-in">
