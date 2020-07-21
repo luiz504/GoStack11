baed69b48ecb9ba835bb413e001dc38b7ba6a7dc
luiz504 <luiz504@hotmail.com>
2020-07-21 20:11:17 -0300

    basic toast layout

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -5,11 +5,13 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp'; //eslint-disable-line
 
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
+      <ToastContainer />
     </AuthProvider>
 
     <GlobalStyles />
