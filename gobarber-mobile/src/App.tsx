190745e0ad1528f3087e71e5ada23d3d28d6def8
luiz504 <luiz504@hotmail.com>
2020-07-23 19:33:35 -0300

    adjus background/statusbar

--- gobarber-mobile/src/App.tsx
+++ gobarber-mobile/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text> Hello Word</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Text> Hello Word</Text>
+      </View>
+    </>
   );
 };
 
