826e6ddd20053b5883676dc4011d6388b6ea2f0d
luiz504 <luiz504@hotmail.com>
2020-07-26 11:48:42 -0300

    sign in llayout

--- gobarber-mobile/src/pages/SignIn/index.tsx
+++ gobarber-mobile/src/pages/SignIn/index.tsx
@@ -1,31 +1,81 @@
 import React from 'react';
-import { Image, Alert } from 'react-native';
+import {
+  Image,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { ScrollView } from 'react-native-gesture-handler';
+import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { Container, Title } from './styles';
 
-import logoImg from '../../assets/logo.png';
+import {
+  Container,
+  Title,
+  ForgotPW,
+  ForgotPWText,
+  SignUpBtn,
+  SignUpBtnText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Title> SignIn</Title>
+            <View>
+              <Title> SignIn</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
 
-      <Input name="password" icon="lock" placeholder="Password" />
+            <Input name="password" icon="lock" placeholder="Password" />
 
-      <Button
+            <Button
+              onPress={() => {
+                Alert.alert('deu');
+              }}
+            >
+              Enter
+            </Button>
+
+            <ForgotPW
+              onPress={() => {
+                Alert.alert('deu');
+              }}
+            >
+              <ForgotPWText>Forgot password</ForgotPWText>
+            </ForgotPW>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <SignUpBtn
         onPress={() => {
           Alert.alert('deu');
         }}
       >
-        Enter
-      </Button>
-    </Container>
+        <>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <SignUpBtnText>Sign Up</SignUpBtnText>
+        </>
+      </SignUpBtn>
+    </>
   );
 };
 
--- gobarber-mobile/src/pages/SignIn/styles.ts
+++ gobarber-mobile/src/pages/SignIn/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
-import { robotoSlabMedium } from '../../styles/fonts';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+import { robotoSlabMedium, robotoSlabRegular } from '../../styles/fonts';
 
 export const Container = styled.View`
   padding: 0 30px;
@@ -14,4 +16,38 @@ export const Title = styled.Text`
   color: #f4ede8;
   font-family: ${robotoSlabMedium};
   margin: 64px 0 24px;
+`;
+
+export const ForgotPW = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPWText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: ${robotoSlabRegular};
+`;
+
+export const SignUpBtn = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SignUpBtnText = styled.Text`
+  margin-left: 16px;
+
+  color: #ff9000;
+  font-size: 18px;
+  font-family: ${robotoSlabRegular};
 `;
