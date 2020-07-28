48d4e2a01c1ea3fe0d494744323e8ba063ade433
luiz504 <luiz504@hotmail.com>
2020-07-28 19:08:31 -0300

    sliting infra

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,20 +1,20 @@
-import "dotenv/config";
-import "reflect-metadata";
+import 'dotenv/config';
+import 'reflect-metadata';
 
-import express, { Request, Response, NextFunction } from "express";
-import "express-async-errors";
-import cors from "cors";
-import routes from "./routes";
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import routes from './routes';
 
-import "./database";
-import uploadConfig from "./config/upload";
-import AppError from "./errors/AppError";
+import './database';
+import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/files", express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 // global error handling
@@ -22,18 +22,18 @@ app.use(
   (err: Error, resquest: Request, response: Response, _: NextFunction) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
-        status: "error",
+        status: 'error',
         message: err.message,
       });
     }
-    if (process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV === 'development') {
       console.error(err); //eslint-disable-line
     }
     return response.status(500).json({
-      status: "error",
-      message: "Internal server error",
+      status: 'error',
+      message: 'Internal server error',
     });
-  }
+  },
 );
 
 app.listen(3333, () => {
